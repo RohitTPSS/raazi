28a48cf87099c8ef4df6994a7364fbe0914d4e2b
shammitpss <shammitpss@gmail.com>
2023-02-23 08:56:06 +0530

    some design work

---
+++ custom.js
@@ -0,0 +1,13 @@
+/*SEARCH POP-UP*/
+
+function myFunction() {
+    var popper = document.querySelector("body");
+    popper.classList.add("show");
+}
+
+function myFunction1() {
+    let popper = document.querySelector("body");
+    popper.classList.remove("show");
+}
+
+/*SEARCH POP-UP*/
